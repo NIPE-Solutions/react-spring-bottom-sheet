481932cb3cb572cff78a3325017686361ddaaaa3
Nicholas Petrasek <nicholas.petrasek@gmail.com>
2026-09-02 20:25:54 +0200

    refactor: remove legacy version 4 source

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,9 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     environment: 'jsdom',
-    include: [
-      'src/{accessibility,components,composition,context,controller,gestures,layout,motion}/**/*.{test,spec}.{ts,tsx}',
-    ],
+    include: ['src/**/*.{test,spec}.{ts,tsx}'],
     setupFiles: ['./test/setup.ts'],
   },
 })
