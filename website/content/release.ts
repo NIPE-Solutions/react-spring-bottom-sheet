085a5f06db40fa44686cc9f0a7693bb9432b248e
Nicholas Petrasek <nicholas.petrasek@gmail.com>
2026-09-09 12:08:22 +0200

    docs: mark 5.0.2 as published

--- website/content/release.ts
+++ website/content/release.ts
@@ -1,6 +1,12 @@
 export function getReleasePresentation(version: string) {
   const prerelease = version.includes('-')
-  const publishedVersions = new Set(['4.1.0', '5.0.0-alpha.0', '5.0.0'])
+  const publishedVersions = new Set([
+    '4.1.0',
+    '5.0.0-alpha.0',
+    '5.0.0',
+    '5.0.1',
+    '5.0.2',
+  ])
   return {
     channel: prerelease ? ('next' as const) : ('latest' as const),
     installCommand: `npm install @nipe-solutions/react-spring-bottom-sheet${
